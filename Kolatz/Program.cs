5bdad38621bd729cedb40b8e63cf7743c453654c
Sanela <sanela.terzic@hotmail.rs>
2019-10-27 14:39:55 +0100

    Add project files.

---
+++ Kolatz/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kolatz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite broj:");
+            
+            int broj = int.Parse(Console.ReadLine());
+
+            while (broj != 1)
+            {
+                if (broj % 2 == 0)
+                {
+                    broj = broj / 2;
+                    Console.WriteLine(broj);
+                 
+                }
+                else
+                {
+                    broj = broj * 3 + 1;
+                    Console.WriteLine(broj);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
